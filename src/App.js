d5f4eda6dc998713104516dab56147de15ea8da6
Shaurya-Srivastav <ssrivastav@ucdavis.edu>
2024-02-21 10:50:24 -0800

    adding history paeg

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import React from 'react';
 import SignUpPage from './components/Sign-Up-Page/SignUpPage';
 import SearchPage from './components/Search-Page/SearchPage';
-
+import HistoryPage from './components/History-Page/HistoryPage';
 
 function App() {
   return (
     <div className="App">
       {/*<SignUpPage />*/}
-      <SearchPage />
+      {/* <SearchPage /> */}
+      <HistoryPage />
     </div>
   );
 }
---
+++ src/components/History-Page/HistoryPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HistoryPage.css';
+
+function HistoryPage() {
+  return (
+    <div className="history-page">
+        <h1>History Page</h1>
+    </div>
+  );
+}
+
+export default HistoryPage;
